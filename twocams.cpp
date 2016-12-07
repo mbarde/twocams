eabe0bb97ba38597f6014181b790b39777cd0621
Matthias Barde <mbarde@uni-koblenz.de>
2016-12-07 11:10:24 +0100

    Create projection matrix for cams and set markers

--- twocams.cpp
+++ twocams.cpp
@@ -12,6 +12,35 @@ static void calcBoardCornerPositions(Size boardSize, float squareSize, vector<Po
             corners.push_back(Point3f(j*squareSize, i*squareSize, 0));
 }
 
+void createProjectionMatrix(Mat tvec, Mat rvec, Mat& proj) {
+  Mat rmat;
+  Rodrigues(rvec, rmat); // create rotation matrix from rotation vector
+  hconcat(rmat, Mat(tvec), proj); // concat rotation matrix and translation vector P = [R|t]
+}
+
+Point2i cam0point;
+Point2i cam1point;
+
+void mouseCam0Handler(int event, int x, int y, int flags, void* param)
+{
+    switch(event){
+    case CV_EVENT_LBUTTONDOWN:
+        cam0point = Point2i(x,y);
+        cout << cam0point << endl;
+        break;
+    }
+}
+void mouseCam1Handler(int event, int x, int y, int flags, void* param)
+{
+    switch(event){
+    case CV_EVENT_LBUTTONDOWN:
+        cam1point = Point2i(x,y);
+        cout << cam1point << endl;
+        break;
+    }
+}
+
+
 int main(int, char**)
 {
     VideoCapture cap0(0);
@@ -26,7 +55,11 @@ int main(int, char**)
     namedWindow("cam0", 1);
     namedWindow("cam1", 1);
 
-    Mat frame0, frame1;
+    int mouseParam= CV_EVENT_FLAG_LBUTTON;
+    setMouseCallback("cam0",mouseCam0Handler,&mouseParam);
+    setMouseCallback("cam1",mouseCam1Handler,&mouseParam);
+
+    Mat frame0, frame1, proj0, proj1;
     Size boardSize(9, 6);
     float squareSize = 25;
 
@@ -63,9 +96,10 @@ int main(int, char**)
                        distCoeffs, rvec, tvec, false);
 
           cout << norm(tvec) << endl;
+          createProjectionMatrix(tvec, rvec, proj0);
           calibrated0 = true;
 
-          cout << "Got cam0 pos and rot!" << endl;
+          cout << "Got cam0 projection matrix!" << endl;
           cout << "Trying to get cam1 pos and rot" << endl;
         }
       }
@@ -88,12 +122,21 @@ int main(int, char**)
                        distCoeffs, rvec, tvec, false);
 
           cout << norm(tvec) << endl;
+          createProjectionMatrix(tvec, rvec, proj1);
           calibrated1 = true;
-          cout << "Got cam1 pos and rot!" << endl;
+          cout << "Got cam1 projection matrix!" << endl;
           break;
         }
 
         if(waitKey(30) >= 0) break;
+      }
+
+      // draw markers
+      if (cam0point.x != 0) {
+        circle( frame0, cam0point, 3, Scalar(0,255,0), -1, 8, 0 );
+      }
+      if (cam1point.x != 0) {
+        circle( frame1, cam1point, 3, Scalar(0,255,0), -1, 8, 0 );
       }
 
       imshow("cam0", frame0);
@@ -101,7 +144,7 @@ int main(int, char**)
 
       char key = (char)waitKey(30);
       if( key  == 32 ) {
-        start = true;
+        if (!start) start = true;
       } else if (key > 0) {
         break;
       }
